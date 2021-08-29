e77dd41521bdc03df87369d2c0295dcb972b4837
Reetu <rajreeturaj103@gmail.com>
2021-08-29 12:37:06 +0530

    initial commit

---
+++ client/src/components/Posts/Pot/Pot.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import useStyles from './styles';
+import { Card, CardActions, CardMedia, Button, Typography,CardContent } from '@material-ui/core';
+import ThumbUpAltIcon from '@material-ui/icons/ThumbUpAlt';
+import DeleteIcon from '@material-ui/icons/Delete';
+import moment from 'moment';
+import MoreHorizIcon from '@material-ui/icons/MoreHoriz';
+const Pot = ({post}) => {
+    const classes = useStyles();
+    return (
+        <Card className={classes.card}>
+            <CardMedia className={classes.media} image={post.selectedFile} title={post.title}/>
+            <div className={classes.overlay}>
+                <Typography variant="h6">{post.creator}</Typography>
+                <Typography variant="body2">{moment(post.createdAt).fromNow()}</Typography>
+            </div>
+            <div className={classes.overlay2}>
+                <Button style={{color:'white'}} size="small" onClick={()=>{}}>
+                    <MoreHorizIcon fontSize="default" />
+                </Button>
+            </div>
+            <div className={classes.details}>
+                <Typography variant="body2" color="textSecondary">{post.tags.map((tag)=>'#')}</Typography>
+            </div>
+        </Card>
+    );
+}
+export default Pot;
