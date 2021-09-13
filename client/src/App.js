f443fcea5be8f38c576875fc7b84f402c8353eea
Reetu <rajreeturaj103@gmail.com>
2021-09-13 17:04:37 +0530

    like post function and font changes

--- client/src/App.js
+++ client/src/App.js
@@ -25,7 +25,7 @@ const App = ( ) =>{
             <Grow in>
                 <Container>
                     <Grid container justify= "space-between" alignment="stretch" spacing={3}>
-                        <Grid item xs={12} sm={4}>
+                        <Grid item xs={12} sm={6}>
                             <Posts setCurrentId={setCurrentId} />
                         </Grid>
                         <Grid item xs={12} sm={4}>
--- client/src/components/Posts/Post/Post.js
+++ client/src/components/Posts/Post/Post.js
@@ -6,7 +6,7 @@ import DeleteIcon from '@material-ui/icons/Delete';
 import moment from 'moment';
 import MoreHorizIcon from '@material-ui/icons/MoreHoriz';
 import { useDispatch } from 'react-redux';
-import {deletePost} from '../../../actions/posts';
+import {deletePost,likePost} from '../../../actions/posts';
 
 
 const Post = ({post, setCurrentId}) => {
@@ -32,10 +32,10 @@ const Post = ({post, setCurrentId}) => {
             </div>
             <Typography className={classes.title} variant="h5" gutterBottom>{post.title}</Typography> 
             <CardContent>
-                <Typography variant="h5" gutterBottom>{post.message}</Typography>
+                <Typography variant="body2" color="textSecondary" component="p" >{post.message}</Typography>
             </CardContent>
             <CardActions className={classes.cardActions}>
-                <Button size="small" color="primary" onClick={()=>{}}>
+                <Button size="small" color="primary" onClick={()=>dispatch(likePost(post._id))}>
                     <ThumbUpAltIcon fontSize="small" />
                     Like
                     {post.likeCount}
